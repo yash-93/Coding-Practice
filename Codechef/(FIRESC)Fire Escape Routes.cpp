89d1b1ab41d5763a54aec6e2257ab2d654f4e8f9
yash-93 <yashdeepbachhas93@gmail.com>
2020-05-23 02:39:57 +0530

    Update (FIRESC)Fire Escape Routes.cpp

--- Codechef/(FIRESC)Fire Escape Routes.cpp
+++ Codechef/(FIRESC)Fire Escape Routes.cpp
@@ -1,86 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<bool> visited;
-vector<int> component_len;
-int routes = 0, len = 0;
+#define mod 1000000007
 
-void add_edge(vector<pair<int,int> > adj_list[], int u, int v, int weight)
+vector<bool> visited;
+
+long long int routes = 0, len = 0;
+long long int route_cap_ways = 1;
+
+void add_edge(vector<long long int> adj_list[], long long int u, long long int v)
 {
-    adj_list[u].push_back(make_pair(v, weight));
-    adj_list[v].push_back(make_pair(u, weight));
+    adj_list[u].push_back(v);
+    adj_list[v].push_back(u);
 }
 
-void printGraph(vector<pair<int,int> > adj_list[], int V){
-    int v, w;
-    for (int u = 0; u < V; u++)
+void printGraph(vector<long long int> adj_list[], long long int V){
+    long long int v, w;
+    for (long long int u = 1; u <= V; u++)
     {
         cout << "Node " << u << ": ";
         for (auto it = adj_list[u].begin(); it!=adj_list[u].end(); it++)
         {
-            v = it->first;
-            w = it->second;
-            cout << "(" << v << ", " << w << ")" << "->";
+            v = *it;
+            cout << v << "->";
         }
         cout << "\n";
     }
 }
 
-void DFS(vector<pair<int,int> > adj_list[], int u){
+void DFS(vector<long long int> adj_list[], long long int u){
     len++;
     visited[u] = true;
-    cout << u << " ";
 
     for (auto it = adj_list[u].begin(); it!=adj_list[u].end(); it++)
     {
-        if(!visited[it->first])
-            DFS(adj_list, it->first);
+        if(!visited[*it])
+            DFS(adj_list, *it);
     }
 }
 
 int main(){
-    int V;
-    cout << "Enter number of vertices: ";
-    cin >> V;
-    visited.assign(V, false);
-    vector<pair<int,int> > adj_list[V];
-    int edges;
-    cout << "Enter no of edges you want to have: ";
-    cin >> edges;
-    int max_edges = (V*(V-1))/2;
-    if( edges > max_edges ){
-        cout << "Max edge limit exceeded. Exiting...";
-        exit(0);
-    }
-    else{
-        int u, v, weight;
-        for(int i=0; i<edges; i++){
-            cout << "Enter vertex endpoints and weight(u v weight): ";
-            cin >> u >> v >> weight;
-            add_edge(adj_list, u, v, weight);
+    int T; cin >> T;
+    long long int N, M;
+    while (T--)
+    {
+        cin >> N >> M;
+        visited.assign(N+1, false);
+        vector<long long int> adj_list[N+1];
+        long long int u, v;
+        for (long long int i=1; i<=M; i++){
+            cin >> u >> v;
+            add_edge(adj_list, u, v);
         }
-        cout << "\n";
-        //printGraph(adj_list, V);
-        //cout << "\n";
 
-        cout << "All Connected Components are as follows : \n";
-        for (int i=0; i<V; i++){
+        vector<long long int> component_len;
+        for (long long int i=1; i<=N; i++){
             if (visited[i] == false){
                 ++routes;
                 DFS(adj_list, i);
-                cout << "\n";
                 component_len.push_back(len);
                 len = 0;
             }
         }
 
-        cout << "\n";
-        cout << routes << " ";
-        long route_cap_ways = 1;
         for(auto it=component_len.begin(); it!=component_len.end(); it++){
-            route_cap_ways *= *it;
+            route_cap_ways = (route_cap_ways * *it) % mod;
         }
-        cout << route_cap_ways;
+        cout << routes << " " << route_cap_ways << "\n";
+        routes = 0;
+        route_cap_ways = 1;
+
     }
     return 0;
 }
