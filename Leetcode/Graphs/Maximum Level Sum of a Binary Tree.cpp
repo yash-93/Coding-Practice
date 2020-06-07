365726f2055d656dde7a9c6492656f861c251ad3
yash-93 <yashdeepbachhas93@gmail.com>
2020-06-07 16:25:29 +0530

    Week 1

---
+++ Leetcode/Graphs/Maximum Level Sum of a Binary Tree.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+
+    }
+};
+
+int main(){
+    Solution s;
+    int ar = {1, 7, 0, 7, -8};
+
+}
