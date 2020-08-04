9efdc48747c3d0f7e909e7c2a97124299bd9d874
yash-93 <yashdeepbachhas93@gmail.com>
2020-08-04 13:59:22 +0530

    Create 342. Power of Four.cpp

---
+++ Leetcode/August Challenge 2020/Week 1/342. Power of Four.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    
+    float logn(int n, int r) 
+    { 
+        return log(n) / log(r); 
+    }
+    
+    bool isPowerOfFour(int n) {
+        if(n == 0)
+            return false;
+        
+        return floor(logn(n,4))==ceil(logn(n,4));
+    }
+};
