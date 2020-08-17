9a0e7c59ee851c4194b55c21556e0c252b5fad77
yash-93 <yashdeepbachhas93@gmail.com>
2020-08-17 18:33:53 +0530

    Create (CHEFWED)Chef and Wedding Arrangements.cpp

---
+++ Codechef/August Long 2020/(CHEFWED)Chef and Wedding Arrangements.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void test(){
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n+1);
+    map<int,int> mp;
+    ll ans=0, vc=0, t=1;
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    int col[n+1][n+1];
+
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=n; j++){
+            col[i][j] = 0;
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        mp.clear();
+        for(int j=i; j<n; j++){
+            col[i][j] = (j==0)?0:col[i][j-1];
+            if(mp.count(a[j])){
+                if(mp[a[j]]==1){
+                    col[i][j]++;
+                }
+                col[i][j]++;
+            }
+            mp[a[j]]++;
+        }
+    }
+
+    ans = 1e18;
+    int table = 100;
+    ll dp[101][101] = {0};
+    for(int i=0; i<=table;i++){
+        for(int j=0;j<=table; j++){
+            dp[i][j] = 0;
+        }
+    }
+
+    for(ll i=0; i<=n; i++)
+        dp[1][i] = col[0][i-1];
+
+    for(ll i=2; i<=table; i++)
+        dp[i][1] = 0;
+
+    for(int i=2; i<=table; i++){
+        for(ll j=2; j<=n; j++){
+            ll best = 1e18;
+            for(ll p=1; p<j; p++){
+                best = min(best, dp[i-1][p]+col[p][j-1]);
+            }
+            dp[i][j] = best;
+        }
+    }
+
+    for(table=1; table<=100; table++){
+        ans = min(table*k+dp[table][n], ans);
+    }
+    cout << ans << endl;
+}
+
+int main(){
+    int t; cin >> t;
+    while(t--)
+        test();
+
+    return 0;
+}
