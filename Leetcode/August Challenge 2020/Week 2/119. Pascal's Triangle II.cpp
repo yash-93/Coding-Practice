22b4cec781df0880cd94849830b926ee996e149b
yash-93 <yashdeepbachhas93@gmail.com>
2020-08-18 00:52:10 +0530

    Create 119. Pascal's Triangle II.cpp

---
+++ Leetcode/August Challenge 2020/Week 2/119. Pascal's Triangle II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res(rowIndex+1,0);
+        res[0] = 1;
+        int temp=0;
+        for(int i=1; i<=rowIndex; i++){
+            for(int j=i; j>0; j--){
+                res[j] += res[j-1];
+            }
+        }
+        
+        return res;
+    }
+};
