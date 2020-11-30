ee2038892c49d4c8be050f83e4e2c6df7a97c261
yash-93 <yashdeepbachhas93@gmail.com>
2020-12-01 02:43:19 +0530

    Create A.cpp

---
+++ Codeforces/Educational Codeforces Round 99/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void test(){
+    string s;
+    cin >> s;
+    int len = s.size();
+
+    cout << len << endl;
+}
+
+int main(){
+    int t; cin >> t;
+    while(t--)
+        test();
+    return 0;
+}
